096289fce89499d4e8010a01898b3bbb08201f21
adriana <adrihenriques289@gmail.com>
2023-02-11 13:15:30 -0300

    teste

---
+++ python_basico/modelo02/aula01.py
@@ -0,0 +1,10 @@
+idade = int(input("digite sua idade:>"))
+
+#if elif else formam a nossa estrutura de condiçao 
+#com essa estrutura conseguimos
+if idade == 18:
+    print("O Aluno tem 18")
+elif idade >= 18:
+    print("O é menor que 18")
+else:
+     print("adios muchachos")
